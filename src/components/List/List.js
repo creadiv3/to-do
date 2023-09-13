5a3dccd36830bfa74e52d7811eb11d6dab640e50
creadive <creadiv3@gmail.com>
2023-09-13 20:38:27 +0200

    Add State to List Component and take data form dataStore.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,26 +3,35 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import Column from '../Column/Column';
 import PropTypes from 'prop-types';
+import { settings } from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  };
+
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node,
+    description: PropTypes.node,
+    columns: PropTypes.array,
   };
 
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
   };
 
   render() {
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} image={this.props.image} />
-        <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.description}>
+          {ReactHtmlParser(this.props.description)}
+        </div>
         <div className={styles.columns}>
-          <Column title="Animals" />
-          <Column title="Plants" />
-          <Column title="Minerals" />
+          {this.state.columns.map(({ key, ...columnProps }) => (
+            <Column key={key} {...columnProps} />
+          ))}
         </div>
       </section>
     );
